964705a15c7887a604b90fa60847597571df5ab9
Harun <haruneryasar21@gmail.com>
2026-07-14 15:40:51 +0300

    İlk commit

---
+++ 02_Variables_Değişkenler/Program.cs
@@ -0,0 +1,183 @@
+﻿#region Double Değişkenler
+
+// double number;
+
+// number = 4.85;
+
+// Console.WriteLine(number);
+
+/*
+Console.WriteLine("***Fiyat Listesi***");
+Console.WriteLine("-------------------");
+
+double applePrice , bananaPrice , orangePrice , watermelonPrice;
+
+applePrice = 5.50;
+bananaPrice = 3.75;
+orangePrice = 4.25;
+watermelonPrice = 12.50;    
+
+Console.WriteLine("Elma Birim Fiyatı : " + applePrice + " TL");
+Console.WriteLine("Muz Birim Fiyatı : " + bananaPrice + " TL");
+Console.WriteLine("Portakal Birim Fiyatı : " + orangePrice + " TL");
+Console.WriteLine("Karpuz Birim Fiyatı : " + watermelonPrice + " TL");
+Console.WriteLine("-------------------");
+
+double applegram , bananagram , orangegram , watermelongram;   
+
+applegram = 1.245;
+bananagram = 0.875;
+orangegram = 0.950;
+watermelongram = 5.500;
+
+double appleTotalPrice = applePrice * applegram;
+double bananaTotalPrice = bananaPrice * bananagram;
+double orangeTotalPrice = orangePrice * orangegram;     
+double watermelonTotalPrice = watermelonPrice * watermelongram;
+
+Console.WriteLine("Elma Toplam Fiyatı : " + appleTotalPrice + " TL");
+
+
+*/
+
+#endregion
+
+#region Char Değişkenler
+
+//ABCDEF
+//DEF...
+//TOPLANTI SAAT 11'DE
+//'' 
+
+//char symbol;
+//symbol = 'a';
+
+//Console.WriteLine(symbol);
+
+#endregion
+
+#region Klavyeden veri girişleri Strinng Değişkenler
+
+
+/*
+
+Console.WriteLine("****   CSharp Hava Yolları Yolcu Bilgisi ****");
+Console.WriteLine();
+
+string passengerName, passengerSurname, passengerAge, passengerDistrict, passengerCity, passengerIdentityNumber;
+
+Console.Write("Yolcu Adı : ");
+passengerName = Console.ReadLine()!;
+
+Console.Write("Yolcu Soyadı : ");
+passengerSurname = Console.ReadLine()!;
+
+Console.WriteLine();
+
+Console.Write("Yolcu Yaşı : ");
+passengerAge = Console.ReadLine()!;
+
+Console.Write("Yolcu İlçe : ");
+passengerDistrict = Console.ReadLine()!;
+
+Console.Write("Yolcu İl : ");
+passengerCity = Console.ReadLine()!;
+
+Console.Write("Yolcu TC Kimlik No : ");
+passengerIdentityNumber = Console.ReadLine()!;
+
+
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Yolcu Bilgileri : " + passengerName + " " + passengerSurname + " " + passengerAge + " " + passengerDistrict + " " + passengerCity + " " + passengerIdentityNumber);
+
+*/
+
+#endregion
+
+#region Klavyeden veri girişleri Int Değişkenler ve Dönüşümler
+
+//ABC12D
+
+
+/* 
+
+
+int shoePrice, computerPrice, chairPrice, tvPrice;
+
+shoePrice = 1000;
+computerPrice = 20000;
+chairPrice = 5000;
+tvPrice = 12000;
+ 
+ int shoeCount, computerCount, chairCount, tvCount;
+
+Console.Write("Lütfen Aldığınız Ayakkabı Adetini Giriniz : ");
+shoeCount = int.Parse(Console.ReadLine()!);
+
+Console.Write("Lütfen Aldığınız Bilgisayar Adetini Giriniz : ");
+computerCount = int.Parse(Console.ReadLine()!);
+
+Console.Write("Lütfen Aldığınız Sandalye Adetini Giriniz : ");
+chairCount = int.Parse(Console.ReadLine()!);    
+
+Console.Write("Lütfen Aldığınız Televizyon Adetini Giriniz : ");
+tvCount = int.Parse(Console.ReadLine()!);   
+
+int totalPrice = shoePrice * shoeCount + computerPrice * computerCount + chairPrice * chairCount + tvPrice * tvCount;
+
+Console.WriteLine();
+
+Console.WriteLine("Toplam Fiyat : " + totalPrice + " TL");
+
+*/
+
+#endregion
+
+
+#region klavyeden veri girişleri ondalıklı sayılar 
+
+/*
+
+double exam1, exam2, exam3, result;
+
+Console.Write("Litfen 1. Sınav Notunu Giriniz : ");
+exam1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Litfen 2. Sınav Notunu Giriniz : ");
+exam2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Litfen 3. Sınav Notunu Giriniz : ");
+exam3 = double.Parse(Console.ReadLine()!); 
+
+result = (exam1 + exam2 + exam3) / 3;
+
+Console.WriteLine();
+
+Console.WriteLine("Sınav Ortalamanız : " + result);
+
+*/
+#endregion
+
+
+#region Klavyeden Karakter Girişleri
+
+/* 
+
+char gender;
+
+Console.Write("Lütfen Cinsiyetinizi Giriniz (E/K) : ");
+gender = char.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Seçtiğiniz Cinsiyet : " + gender);
+
+
+*/
+#endregion
+
+
+
+
+
+
+
